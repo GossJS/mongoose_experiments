b74660a4e7f9652917c1f6ea9ddd2bf90ac1d722
Ilya Goss <goss@corp.ifmo.ru>
2019-11-19 18:00:49 +0300

    Create model.js

---
+++ model.js
@@ -0,0 +1,22 @@
+const mg = require('mongoose'); // 4.11.7
+ 
+const options = [`mongodb://reader:123321@kodaktor.ru/readusers`, 
+	 { useNewUrlParser: true }, 
+	 e => e ? console.error('Соединиться с БД не удалось. На этом всё.') || process.exit(1) : console.log('Успех!')
+];
+
+mg
+.Promise = global
+.Promise; 
+
+module
+.exports = mg
+.createConnection(...options)
+.on('connected', () => console.log('Соединились!'))
+.model('User', new mg.Schema(require('./UserSchema.js')));
+
+// мы можем экспортировать так, ибо монгуз внутренне отслеживает состояние соединения
+// встретив вызов createConnection, он попробует выполнить его 
+// но если соединение не готово, отложит в очередь на событие успешного соединения
+// т.е. можно сказать что коллбэк connected неявно вызывает model 
+// ... и модель становится доступной когда это свершается
